e8c73fa23f8a72adb844ee77b3e62899cfa8054f
Sanjit M A <152069563+sanjitma@users.noreply.github.com>
2025-07-22 16:31:34 +0530

    Update dac_mdejoiner.h

--- dac_mdejoiner.h
+++ dac_mdejoiner.h
@@ -1,19 +1,15 @@
-#ifndef QM_JOIN_H
-#define QM_JOIN_H
+#ifndef DAC_JOIN_H
+#define DAC_JOIN_H
 
 #include <stdint.h>
 
-typedef float data_t;
+typedef float data_ty;
 
+// Multi-bit DAC with channel select (8-bit output)
 // Wrapped with #ifdef __CUDACC__ to ensure compatibility with both host and device compilers
 #ifdef __CUDACC__
 __host__ __device__
 #endif
-void nco(uint8_t &phase, uint8_t phase_inc, data_t &cos_lo, data_t &sin_lo);
-
-#ifdef __CUDACC__
-__host__ __device__
-#endif
-data_t digital_qm(data_t I, data_t Q, data_t cos_lo, data_t sin_lo);
+void dac_multibit_with_select(data_ty din, data_ty &dout, bool channel_select);
 
 #endif
